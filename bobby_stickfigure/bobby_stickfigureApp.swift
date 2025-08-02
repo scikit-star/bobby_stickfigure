8d4d32539cfe5457741951961abeea4194b71116
Aksharaa Ramesh <aksharaaramesh.computing@gmail.com>
2025-08-02 14:26:03 +0800

    Initial Commit

---
+++ bobby_stickfigure/bobby_stickfigureApp.swift
@@ -0,0 +1,17 @@
+//
+//  bobby_stickfigureApp.swift
+//  bobby_stickfigure
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct bobby_stickfigureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
