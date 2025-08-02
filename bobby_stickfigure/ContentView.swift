404cf350566aed92a15a2f969a0ef218c359b0e7
Ying Hsin <limyinghsin@gmail.com>
2025-08-02 14:54:44 +0800

    I added a lot more emojis

--- bobby_stickfigure/ContentView.swift
+++ bobby_stickfigure/ContentView.swift
@@ -21,6 +21,22 @@ struct ContentView: View {
             EmojiView(symbol: "🧤", size: 50, x: 50,  y: 90)
             EmojiView(symbol: "🧤", size: 50, x: -50,  y: 90)
             EmojiView(symbol: "👙", size: 100, x: 0,  y: -50)
+            EmojiView(symbol: "🐾", size: 60, x: 0,  y: 400)
+            EmojiView(symbol: "🐾", size: 60, x: 0,  y: 250)
+            EmojiView(symbol: "🐾", size: 60, x: 0,  y: 315)
+            EmojiView(symbol: "💪", size: 50, x: -75,  y: -75)
+            EmojiView(symbol: "🐰", size: 20, x: 75,  y: -80)
+            EmojiView(symbol: "🪨", size: 150, x: 0,  y: 150)
+            EmojiView(symbol: "🐽", size: 40, x: 0,  y: -125)
+            EmojiView(symbol: "☄️", size: 100, x: 150,  y: -40)
+
+
+
+
+
+
+
+
         }
         .frame(width: 200, height: 320)
     }
